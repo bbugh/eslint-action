6640c7a1a093bbd97cb15da59f8b155e5d80e90d
Brian Bugh <brainbag@gmail.com>
2019-11-16 09:07:12 -0600

    Don't run yarn/npm, assume another step did it

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,10 +2,4 @@
 
 set -e
 
-if [ -f yarn.lock ]; then
-    yarn --non-interactive --silent --ignore-scripts --production=false --frozen-lockfile --prefer-offline
-else
-    NODE_ENV=development npm install
-fi
-
 NODE_PATH=node_modules node /action/lib/run.js
